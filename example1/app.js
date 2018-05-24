b6a1999adf22984b25bce948f260c961c20c76ce
Harutyun Abgaryan <harutyunabgaryan@gmail.com>
2018-05-24 17:48:14 +0400

    change

--- example1/app.js
+++ example1/app.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
 const PORT = process.env.PORT || 3000
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', (req, res) => res.send('Hello World1'))
 
 app.listen(PORT, () => console.log('Example app listening on port 3000!'))
--- example2/app.js
+++ example2/app.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
 const PORT = process.env.PORT || 3001
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', (req, res) => res.send('Hello World!2'))
 
 app.listen(PORT, () => console.log('Example app listening on port 3000!'))
